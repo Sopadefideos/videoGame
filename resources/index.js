dbfa45b7e06c067bd8cfeb4717b435006745491f
robertoceuta <robertopoyato@outlook.com>
2021-11-17 13:06:07 +0100

    sectionsYnuevas imagenes

--- resources/index.js
+++ resources/index.js
@@ -10,3 +10,25 @@ function autoPlayYouTubeModal(){
       });   
     });
   }
+  
+  let tl = gsap.timeline({
+    scrollTrigger: {
+      trigger: ".paginaUno",
+      start: "0%",
+      end: "100%",
+      markers:true,
+      scrub:true,
+      pin:true,
+
+    },
+  });
+  tl.fromTo(".flechaAbajo", {opacity:1,}, {opacity:0, duration: 1});
+  tl.fromTo(".paginaUno", 
+  {clipPath:'circle(8%)' }, 
+  {clipPath:'circle(100%)', duration:8});
+
+  tl.fromTo(".logoFiT", {scale:1}, {scale:0, opacity:0, duration:4});
+
+  tl.fromTo(".titulo", {opacity:0, scale:0}, {opacity:1,scale:1, duration:4});
+  tl.fromTo(".subtitulo", {opacity:0}, {opacity:1, duration:4});
+  
